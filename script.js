a1704e38ca334b1823a3d55d8eccd261b54754c9
Krishna-kant-patel-3112 <krishnakantpatel101@gmail.com>
2026-02-16 10:42:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Progress Bar Animation
+window.addEventListener("load", () => {
+    document.querySelectorAll(".progress-bar").forEach(bar => {
+        let width = bar.getAttribute("data-width");
+        bar.style.width = width;
+    });
+});
